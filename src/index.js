9250b4875898990a797c2d93cc4e4e7aff9ceb0e
Victor Mendez <victor.mmc.pl@hotmail.com>
2022-12-08 10:01:33 -0600

    Init server.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 //Codigo del servidor
+const express = require('express')
+const app = express()
+
+//Settings
+
+//Middleware
+
+//Routes
+
+//Static files
+
+//Starting server
+app.listen(5173, () => {
+    console.log('server on port 5173')
+})
