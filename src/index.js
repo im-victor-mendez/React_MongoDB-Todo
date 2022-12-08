f58bb9a30f09c7ad485c55e08723d2f1f3c97932
Victor Mendez <victor.mmc.pl@hotmail.com>
2022-12-08 10:20:17 -0600

    Implemented Middlewares.

--- src/index.js
+++ src/index.js
@@ -2,12 +2,18 @@
 const express = require('express')
 const app = express()
 
+const morgan = require('morgan')
+
 //Settings
 
 //Servicio de la nube
 app.set('port', process.env.PORT || 5173)
 
 //Middleware
+app.use(morgan('dev'))
+
+//Cada que llegue un dato al servidor, llegará a esta función y si es formato JSON lo podremos utilizar en la app.
+app.use(express.json())
 
 //Routes
 
