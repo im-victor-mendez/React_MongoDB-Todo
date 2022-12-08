4a1d45af9bfaaa46fefc535140b9637bd9fd85e2
Victor Mendez <victor.mmc.pl@hotmail.com>
2022-12-08 12:15:54 -0600

    Defined Task structure.

--- src/models/task.js
+++ src/models/task.js
@@ -1,1 +1,15 @@
 //Modelo de tarea para usar en MongoDB
+const mongoose = require('mongoose')
+
+const { Schema } = mongoose
+
+const TaskSchema = new Schema({
+    title: {type: String, required: true},
+    description: {type: String, required: true}
+})
+
+/**
+ * 'Task': Como voy a poder reutilizarlo dentro de la aplicación
+ * TaskSchema: La estructura que utilizará
+ */
+module.exports = mongoose.model('Task', TaskSchema)
