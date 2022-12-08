721ff93005442a0820723783a766c521498043f2
Victor Mendez <victor.mmc.pl@hotmail.com>
2022-12-08 12:27:48 -0600

    Implemented Task.

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -3,12 +3,13 @@ const express = require('express')
 //Devuelve un objeto en el cual poder ingresar rutas
 const router = express.Router()
 
+const Task = require('../models/task')
+
 //Cuando se ingrese a esta dirección, mandará con req a la base de datos o reqresará con res
 //para mostrarlo en pantalla.
-router.get('/', (req, res) => {
-    res.json({
-        status: 'API works'
-    })
+router.get('/', async (req, res) => {
+    const tasks = await Task.find()
+    res.json(tasks)
 })
 
 module.exports = router
