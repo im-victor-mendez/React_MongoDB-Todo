1dc5fc6090e60b9349a95c847c5c0c81791adffe
Victor Mendez <victor.mmc.pl@hotmail.com>
2022-12-08 21:02:33 -0600

    Fixed warning for React.StrictMode.

--- src/app/index.js
+++ src/app/index.js
@@ -3,4 +3,8 @@ import { render } from "react-dom";
 
 import App from "./App";
 
-render(<App />, document.getElementById('root'))
+render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+, document.getElementById('root'))
