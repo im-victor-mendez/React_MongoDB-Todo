cebbd4a99a01e70553da3850ea0832c900f1dcbf
Victor Mendez <victor.mmc.pl@hotmail.com>
2022-12-08 20:10:04 -0600

    Configured loader.

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,24 @@
 module.exports = {
-    entry: './src/app/main.js',
+    entry: './src/app/index.js',
     output: {
-        path: `${__dirname}/src/public`,
-        filename: 'bundle.js'
+      path: __dirname + '/src/public',
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: [
+                '@babel/preset-env',
+                '@babel/preset-react'
+              ]
+            }
+          }
+        }
+      ]
     }
-}
+  };
