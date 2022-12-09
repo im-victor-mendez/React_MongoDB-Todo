0aa3b7cf6d50fdcbac243db19aa31dbcc01af925
Victor Mendez <victor.mmc.pl@hotmail.com>
2022-12-08 21:02:13 -0600

    Added addTask structure and functions.

--- src/app/App.js
+++ src/app/App.js
@@ -1,9 +1,60 @@
-import React from "react";
+import React, { useState } from "react"
 
 function App() {
+  const [task, setTask] = useState({ title: "", description: "" })
+
+  function handleChange(event) {
+    setTask({
+      ...task,
+      [event.target.name]: event.target.value
+    })
+  }
+
+  function addTask() {
+    fetch('/api/tasks', {
+      method: 'POST',
+      body: JSON.stringify(task),
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+      console.log(task)
+      
+      alert('Task added :D')
+      setTask({ title: '', description: '' })
+    })
+    .catch(err => console.log(err))
+  }
+
   return (
     <div className="App">
-      <h1>Hola mundo :D</h1>
+      <section className="card">
+        <form
+        onSubmit={
+          event => {
+            event.preventDefault()
+
+            addTask(event)
+          }
+        }
+        >
+          <label>
+            <p>Title</p>
+            <input type='text' name="title" onChange={handleChange} value={task.title} />
+          </label>
+
+          <label>
+            <p>Description</p>
+            <textarea name="description" onChange={handleChange} value={task.description}></textarea>
+          </label>
+
+          <button type="submit">Agregar</button>
+        </form>
+      </section>
     </div>
   )
 }
