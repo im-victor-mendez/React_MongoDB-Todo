60f7f00d33f163fa81150125780ae8f77cc2ac41
Victor Mendez <victor.mmc.pl@hotmail.com>
2022-12-14 18:09:45 -0600

    Implemented React Router.

--- src/app/index.js
+++ src/app/index.js
@@ -1,10 +1,15 @@
 import React from "react";
 import { render } from "react-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import App from "./App";
 
 render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 , document.getElementById('root'))
